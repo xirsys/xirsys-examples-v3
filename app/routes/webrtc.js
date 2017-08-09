db4508bd541dae9ffb95292d7959b346a77f67ca
Noe <noenvega@gmail.com>
2017-08-09 15:42:54 -0700

    updates: added override to allow client-side to dynamically set channel name upon request

--- app/routes/webrtc.js
+++ app/routes/webrtc.js
@@ -3,14 +3,35 @@ var https= require('https');
 // Constructor
 function WebRtc(xirsys) {
     var router = express.Router();
-    // check if method is an allowed service
-    router.use(function (req, res, next){
+    // check if method is defined in request
+    router.use('/', function(req, res, next){
         var uri = req.url.split('?').shift();
-        if(xirsys['allowedServices'].indexOf(uri) == -1){
+        console.log(uri, req.params);
+        if(req.params.method == null && uri.length<=1){
             req.error = {
                 "s": "error",
-                "v": "not_allowed"
+                "v": "method_not_found"
             };
+        }
+        next();
+    });
+    // check if method is an allowed service
+    router.use('/:method/', function(req, res, next){
+        if(req.params.method != null){
+            //console.log(xirsys['allowedServices'].indexOf('/'+req.params.method) , req.params.method);
+            if (xirsys['allowedServices'].indexOf('/'+req.params.method) == -1) {
+                req.error = {
+                    "s": "error",
+                    "v": "not_allowed"
+                };
+            }
+        }
+        next();
+    });
+    //check request for allowedClientSetChannel
+    router.use('/:method/:channel',function (req, res, next){
+        if(xirsys['overrideAllowedChannel'] == true && req.params.channel != null){
+            xirsys.info.channel = req.params.channel;
         }
         next();
     });
